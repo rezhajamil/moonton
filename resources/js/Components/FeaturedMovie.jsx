fe8c43ce5524be24b446540a4866bbbe34e0b1f7
John Doe <yamatogija@gmail.com>
2023-01-15 07:37:05 +0700

    feat: slicing dashboard

--- resources/js/Components/FeaturedMovie.jsx
+++ resources/js/Components/FeaturedMovie.jsx
@@ -33,7 +33,7 @@ export default function FeaturedMovie({
             </div>
             {/* bottom detail */}
             <div
-                className="absolute bottom-0 h-[100px] left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
+                className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
                             rounded-br-[28px] flex justify-between items-center px-7 h-[130px]"
             >
                 <div>
